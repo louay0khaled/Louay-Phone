c3b6fc3c52ce9d325b74c3775034b9c22dcd874a
louay0khaled <louay.o.khaled@gmail.com>
2026-08-20 02:20:34 +0300

    feat(pwa): register storefront service worker without touching admin

---
+++ app/components/PWARegister.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { useEffect } from 'react';
+
+export default function PWARegister() {
+  useEffect(() => {
+    if ('serviceWorker' in navigator && location.pathname !== '/admin') {
+      navigator.serviceWorker.register('/sw.js').catch(() => undefined);
+    }
+  }, []);
+
+  return null;
+}
